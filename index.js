e672cd856ec678666d2e71f9fb6bcda4c9dee182
ugrsen <93542523+ugrsen@users.noreply.github.com>
2022-01-02 02:36:18 +0530

    Add files via upload
    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Take 3 inputs from the user and show minimum of the three.
+var num1 = prompt ('enter the num1');
+var num2 = prompt ('enter the num2');
+var num3 = prompt ('enter the num3');
+if(num1 < num2 && num1 < num3) 
+     {
+           console.log("num1  is small");
+     }
+ else if (num2 < num1 && num2 < num3)
+    {
+           console.log("num2  is small");
+        
+     }
+  else
+     {
+
+         console.log("num3 is  small ");
+         
+     }   
---
+++ index1.js
@@ -0,0 +1,42 @@
+//Check whether the given chracter is a vowel or not. (Also check for capital characters).
+/*var char = prompt("enter a char");
+
+if(char  == ("'a||A' || 'E||e' || 'i||I' || 'o||O' || 'u||U'") )
+{
+    console.log( "char is vowel" );
+}
+else
+{
+   console.log (" char is consonent ");
+}
+var char = prompt("enter a char ");
+
+if(char == "a||A") {
+ alert("It is a vowel");
+} else if(char == "e||E") {
+ alert("It is a vowel");
+} else if(char == "i||I") {
+ alert("It is a vowel");
+} else if(char == "o||O") {
+ alert("It is a vowel");
+} else if(char == "u||U") {
+ alert("It is a vowel");
+} else {
+ alert("It is a consonant");
+}*/
+var char = prompt("Enter any character");
+
+
+if((char == "a") || (char == "A")) {
+ alert("It is a vowel");
+} else if((char == "e") || (char == "E")) {
+ alert("It is a vowel");
+} else if((char == "i") || (char == "I")) {
+ alert("It is a vowel");
+} else if((char == "o") || (char == "O")) {
+ alert("It is a vowel");
+} else if((char == "u") || (char == "U")) {
+ alert("It is a vowel");
+} else {
+ alert("It is a consonant");
+}
